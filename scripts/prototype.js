f888206f49057c57cc655b287156a649d4155004
Ben-Kandel <deltaforce126@gmail.com>
2020-11-04 19:13:58 -0500

    updated prototype page

--- scripts/prototype.js
+++ scripts/prototype.js
@@ -1,22 +1,48 @@
 let image = document.getElementById('the-gif');
+let title = document.getElementById('scenario-title');
+let text = document.getElementById('dynamic-text');
 
-let gif1 = "https://media.giphy.com/media/ND6xkVPaj8tHO/giphy.gif";
-let gif2 = "https://media.giphy.com/media/epnHmbfUFaBkk/giphy.gif";
-let gif3 = "https://media.giphy.com/media/11fucLQCTOdvBS/giphy.gif";
+let mappings = {
+    scenario1: {
+        name: "Scenario 1",
+        link: "images/correction.gif",
+        title: "Correction",
+        description: "The system is in the on state and is waiting to detect the vehicle moving from the lane's center. Once the vehicle moves from the lane, a warning light notifies the user, and the vehicle's LMS system returns the vehicle to the center of the lane.",
+    },
+    scenario2: {
+        name: "Scenario 2",
+        link: "images/switching_lanes.gif",
+        title: "Switching Lanes",
+        description: "The system is in the on state, and the driver uses their turn signal to make a lane change. The turn signal disables the system temporarily and allows the vehicle to perform the lane change. Once the lane change has finished, the vehicle’s LMS system is enabled once again. ",
+    },
+    scenario3: {
+        name: "Scenario 3",
+        link: "images/speed.gif",
+        title: "Speed",
+        description: "The system is not able to be used when the vehicle is going under 35 miles per hour (MPH). Once the vehicle accelerates to 35 MPH, the system is turned on.",
+    }
+}
 
 function b1Clicked(){
-    image.src = gif1;
+    title.innerText = mappings.scenario1.name;
+    image.src = mappings.scenario1.link;
+    //testing..
+    text.innerHTML = `<h1>${mappings.scenario1.title}</h1><p1>${mappings.scenario1.description}</p1>`
 }
 
 function b2Clicked(){
-    image.src = gif2;
+    title.innerText = mappings.scenario2.name;
+    image.src = mappings.scenario2.link;
+    text.innerHTML = `<h1>${mappings.scenario2.title}</h1><p1>${mappings.scenario2.description}</p1>`
 }
 
 function b3Clicked(){
-    image.src = gif3;
+    title.innerText = mappings.scenario3.name;
+    image.src = mappings.scenario3.link;
+    text.innerHTML = `<h1>${mappings.scenario3.title}</h1><p1>${mappings.scenario3.description}</p1>`
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log('hello');
-    image.src = gif1;
+    b1Clicked();
 })
